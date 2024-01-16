94f1949895a6ebdedd5633b8e1a63520b9d5e43d
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-01-16 00:24:41 -0800

    update on prime file

---
+++ primetest.py
@@ -0,0 +1,11 @@
+from .isPrime import is_prime
+
+def test_prime(n, expected):
+    if is_prime(n) != expected:
+        print(f"ERROR on is_prime({n}), expected {expected} ut ur out put is {is_prime(n)}")
+
+
+
+test_prime(5, True)
+test_prime(10, False)
+test_prime(25, False)
