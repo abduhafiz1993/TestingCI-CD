0e8a315d2acbfb3fcb76ebdaf0e583efab985c09
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-01-18 07:37:22 -0800

    sone change

--- unittest.py
+++ unittest.py
@@ -8,17 +8,19 @@ class Test(unittest.TestCase):
     def test_1(self):
         self.assertTrue(is_prime(2))
 
-    def test_1(self):
+    def test_2(self):
         self.assertFalse(is_prime(8))
 
-    def test_1(self):
+    def test_3(self):
         self.assertTrue(is_prime(11))
 
-    def test_1(self):
+    def test_4(self):
         self.assertFalse(is_prime(1))
 
-    def test_1(self):
+    def test_5(self):
         self.assertFalse(is_prime(1))
 
-    def test_1(self):
+    def test_6(self):
         self.assertFalse(is_prime(1))
+
+    
