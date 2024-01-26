97c5721ff7f12ec150f5283c8dc4e26f18a59f8a
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-01-26 10:46:45 -0800

    working on personal website

--- unittest.py
+++ unittest.py
@@ -1,26 +1,28 @@
 import unittest
 from isPrime import is_prime
 
-class Test(unittest.TestCase):
+class Tests(unittest.TestCase):
     def test_1(self):
         self.assertFalse(is_prime(1))
-
-    def test_1(self):
-        self.assertTrue(is_prime(2))
 
     def test_2(self):
+        self.assertTrue(is_prime(2))
+
+    def test_8(self):
         self.assertFalse(is_prime(8))
 
-    def test_3(self):
+    def test_11(self):
         self.assertTrue(is_prime(11))
 
-    def test_4(self):
-        self.assertFalse(is_prime(1))
+    def test_25(self):
+        self.assertFalse(is_prime(25))
 
     def test_5(self):
         self.assertFalse(is_prime(1))
 
-    def test_6(self):
+    def test_28(self):
         self.assertFalse(is_prime(1))
 
-    
+
+if __name__ = '__main__':
+    unittest.main()
