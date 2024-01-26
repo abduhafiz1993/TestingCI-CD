ce72e332cc643e4965cabdd2fab2278fe23cde2e
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-01-26 11:18:00 -0800

    fixing bug

--- isPrime.py
+++ isPrime.py
@@ -5,7 +5,7 @@ def is_prime(x):
     if x < 2:
         return False
     
-    for i in range(2, int(math.sqrt(x))):
+    for i in range(2, int(math.sqrt(x))+1):
         if x % i == 0:
             return False
     
