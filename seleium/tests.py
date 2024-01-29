d227bc14766853b3ab6fae8f6e5a8b71de43937e
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-01-29 11:02:17 -0800

    make some change

--- seleium/tests.py
+++ seleium/tests.py
@@ -3,6 +3,7 @@ import pathlib
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Finds the Uniform Resourse Identifier of a file
 def file_uri(filename):
@@ -14,24 +15,30 @@ driver = webdriver.Chrome()
 class WebpageTests(unittest.TestCase):
 
     def test_title(self):
+        """Make sure title is correct"""
+        driver.get(file_uri("counter.html"))
+        self.assertEqual(driver.title, "Counter")
+
+    def test_title(self):
 
         driver.get(file_uri("counter.html"))
-        increase = driver.find_element_by_id("increase")
+        increase = driver.find_element(By.ID, "increase")
         increase.click()
-        self.assertEqual(driver.find_element_by_tag_name("h1").text, "1")
+        self.assertEqual(  h1_text = driver.find_element(By.TAG_NAME, "h1").text, "1")
     
     def test_decrease(self):
+        """Make sure header updated to -1 after 1 click of decrease button"""
         driver.get(file_uri("counter.html"))
-        increase = driver.find_element_by_id("decrease")
-        increase.click()
-        self.assertEqual(driver.find_element_by_tag_name("h1").text, "-1")
+        decrease = driver.find_element(By.ID, "decrease")
+        decrease.click()
+        self.assertEqual(  h1_text = driver.find_element(By.TAG_NAME, "h1").text, "-1")
         
     def test_multiple_increase(self):
         driver.get(file_uri("counter.html"))
-        increase = driver.find_element_by_id("increase")
+        increase = driver.find_element(By.ID, "increase")
         for i in range(3):
             increase.click()
-        self.assertEqual(driver.find_element_by_tag_name("h1").text, "3")
+        self.assertEqual(  h1_text = driver.find_element(By.TAG_NAME, "h1").text, "3")
     
 
 if __name__ == "__main__":
